646213d064f88ea9363b7b32e7f5424f760a2fe8
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-01-24 18:15:23 -0500

    ⚒️ Add section projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import Landing from "@/components/Landing";
 import Description from "@/components/Description";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Landing />
       <Description />
+      <Projects />
     </main>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -22,8 +22,8 @@ export default function Home() {
     gsap.to(button.current, {
       scrollTrigger: {
         trigger: document.documentElement,
-        start: "150px top",
-        end: "bottom bottom",
+        start: 0,
+        end: window.innerHeight,
         onLeave: () => {
           gsap.to(button.current, {
             scale: 1,
