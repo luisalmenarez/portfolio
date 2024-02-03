eefa2d2adb1c6bccdd57ef8e8060be6acd9fcba7
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-02-03 15:11:01 -0500

    💫 Update fixed error page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,37 +8,34 @@ import SlidingImages from "@/components/SlidingImages";
 import Contact from "@/components/Contact";
 import Preloader from "@/components/Preloader";
 import { AnimatePresence } from "framer-motion";
-import LocomotiveScroll from "locomotive-scroll";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const initLocomotiveScroll = async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll({
-        el: document.querySelector(".main"),
-        smooth: true,
-        smoothMobile: true,
-        touch: {
-          enabled: true,
-          sensitivity: 1.5,
-          touchMultiplier: 5,
-        },
-      });
+      if (typeof window !== "undefined") {
+        const LocomotiveScroll = (await import("locomotive-scroll")).default;
+        const locomotiveScroll = new LocomotiveScroll({
+          el: document.querySelector(".main"),
+          smooth: true,
+          smoothMobile: true,
+          touch: {
+            enabled: true,
+            sensitivity: 1.5,
+            touchMultiplier: 5,
+          },
+        });
 
-      setTimeout(() => {
-        setIsLoading(false);
-        document.body.style.cursor = "default";
-        window.scrollTo(0, 0);
-      }, 1500);
+        setTimeout(() => {
+          setIsLoading(false);
+          document.body.style.cursor = "default";
+          window.scrollTo(0, 0);
+        }, 1500);
+      }
     };
 
-    if (typeof window !== "undefined") {
-      initLocomotiveScroll();
-    } else {
-      setIsLoading(false);
-    }
+    initLocomotiveScroll();
   }, []);
 
   return (
