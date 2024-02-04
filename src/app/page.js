2221b8781df51d98434de9badd8fe5a9279999a7
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-02-04 13:50:03 -0500

    💫 Update scroll and slider projects mobile

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
         const locomotiveScroll = new LocomotiveScroll({
           el: document.querySelector(".main"),
           smooth: true,
-          smoothMobile: true,
+          smoothMobile: false,
           touch: {
             enabled: true,
             sensitivity: 1.5,
