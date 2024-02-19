c218128dc6804c0c8e9542aa6acf9a4fed56af7b
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-02-19 05:14:21 -0500

    💫 Add page about

---
+++ src/app/about/components/HeaderAbout/index.jsx
@@ -0,0 +1,38 @@
+import styles from "./style.module.scss";
+import { opacity, slide } from "../../../work/anim";
+import Rounded from "@/common/RoundedButton";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const anim = (variants) => {
+  return {
+    initial: "initial",
+
+    animate: "enter",
+
+    exit: "exit",
+
+    variants,
+  };
+};
+
+const HeaderAbout = () => {
+  return (
+    <article className={styles.content}>
+      <motion.h2 {...anim(opacity)}>
+        Construyendo soluciones que desafían los límites
+      </motion.h2>
+      <motion.div {...anim(slide)} className={styles.strip}>
+        <article className={styles.buttonContainer}>
+          <Link href={"linkedin"}>
+            <Rounded backgroundColor={"#334BD3"} className={styles.button}>
+              <p>Contáctame</p>
+            </Rounded>
+          </Link>
+        </article>
+      </motion.div>
+    </article>
+  );
+};
+
+export default HeaderAbout;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,29 +1,70 @@
 "use client";
 import styles from "./style.module.scss";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import Curve from "@/components/LoaderPage";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { opacity, slide } from "../work/anim";
+import Rounded from "@/common/RoundedButton";
+import Link from "next/link";
+import HeaderAbout from "./components/HeaderAbout";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import { useMediaQuery } from "react-responsive";
 
 const About = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const isMobile = useMediaQuery({ maxWidth: 520 });
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-      document.body.style.cursor = "default";
-      window.scrollTo(0, 0);
-    }, 700);
+    const fetchData = async () => {
+      if (typeof window !== "undefined") {
+        const LocomotiveScroll = (await import("locomotive-scroll")).default;
+        const locomotiveScroll = new LocomotiveScroll({});
+      }
+
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 700);
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <section className={styles.about}>
-      <div className={styles.content}>
-        <h1>ABOUT ME</h1>
-        <AnimatePresence>
-          {isLoading && <Curve pageName="Sobre mí" />}
-        </AnimatePresence>
-      </div>
-    </section>
+    <>
+      <AnimatePresence>
+        {isLoading && <Curve pageName="Sobre mí" />}
+      </AnimatePresence>
+      <section className={styles.about}>
+        <HeaderAbout />
+        <article className={styles.descriptionContent}>
+          <div className={styles.descriptionText}>
+            <p>
+              Soy Luis Almenarez, un arquitecto del código. Explorando el
+              desarrollo de software, busco fusionar creatividad y lógica en
+              cada proyecto. Mi viaje está marcado por la búsqueda constante de
+              la excelencia técnica y la innovación. Para mí, programar es más
+              que una habilidad; es una exploración de los límites de la
+              imaginación en el perpetuo universo digital.
+            </p>
+            <p className={styles.descriptionPhrase}>
+              Descubrimiento constante...
+            </p>
+          </div>
+          <div className={styles.gridImage}>
+            <div
+              data-scroll
+              data-scroll-speed={-0.2}
+              data-scroll-position={"top"}
+              className={styles.overlay}></div>
+          </div>
+        </article>
+      </section>
+      {isMobile ? <Footer /> : <Contact />}
+    </>
   );
 };
 
