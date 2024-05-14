3ba49b4d6161a86ca9c00cb2b6d95caf27f97435
Luis Carlos Almenarez <luisalmenarez0428@gmail.com>
2024-05-14 14:32:55 -0500

    ⚙️ Install Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,21 @@
-import "./globals.css";
-import { Montserrat } from "next/font/google";
-import Header from "../components/header";
-const montserrat = Montserrat({ subsets: ["latin"] });
+import './globals.css';
+import { Montserrat } from 'next/font/google';
+import Header from '../components/header';
+import { Analytics } from '@vercel/analytics/react';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
 export const metadata = {
-  title: "Luis Almenarez - Software Developer",
-  description: "Luis Almenarez - Software Developer",
+  title: 'Luis Almenarez - Software Developer',
+  description: 'Luis Almenarez - Software Developer',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={montserrat.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
